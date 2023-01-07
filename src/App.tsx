4f53f6ac6d17b7f71f8569bff9cc623292ffacad
mohmed <mohmedamged9@gmail.com>
2023-01-07 15:49:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1 className="font-bold text-center text-6xl">Sleep tracker</h1>;
+};
+
+export default App;
---
+++ src/firebase-config.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBddSNCetYEReExwiS3j8mGJta1fHm_HcI",
+  authDomain: "sleep-tracker-47a41.firebaseapp.com",
+  projectId: "sleep-tracker-47a41",
+  storageBucket: "sleep-tracker-47a41.appspot.com",
+  messagingSenderId: "219126237740",
+  appId: "1:219126237740:web:22042e2acc773204021f89",
+  measurementId: "G-QC1LVMG62C",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
