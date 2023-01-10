90f5436e69884c0521e821545f81646db9ce22f1
mohmed <mohmedamged9@gmail.com>
2023-01-10 22:09:16 +0200

    make the UI for the navbar in the dashboard responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,19 +3,18 @@ import { BsPlusLg } from "react-icons/bs";
 const Dashboard = () => {
   return (
     <div>
-      <nav className="flex justify-between p-4 items-center">
-        <button className="btn flex gap-2 items-center bg-blue-500 text-white capitalize">
-          <BsPlusLg /> add new entry
-        </button>
-
-        <div className="flex gap-4 items-center">
+      <nav className="md:flex md:flex-row-reverse justify-between p-4 items-center flex-wrap">
+        <div className="flex gap-4 items-center justify-between">
           <h3 className="text-blue-500 font-semibold capitalize text-xl">
             hi, mohamed amged
           </h3>
-          <button className="btn flex gap-2 items-center bg-blue-500 text-white">
+          <button className=" btn flex gap-2 items-center bg-blue-500 text-white">
             Log out
           </button>
         </div>
+        <button className="my-4 md:m-0 mx-auto btn flex gap-2 items-center bg-blue-500 text-white capitalize">
+          <BsPlusLg /> add new entry
+        </button>
       </nav>
     </div>
   );
